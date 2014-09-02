b1f656dc7ac17f885d59c3b9fee7a06c5fd0c816
Mike Herchel <mike@herchel.com>
2014-09-02 11:57:07 -0400

    Refactored using vh units and less js.

--- scripts.js
+++ scripts.js
@@ -2,10 +2,6 @@ var viewportHeight;
 
 function loadConstants(){
   viewportHeight = Math.ceil(document.getElementById("first").offsetHeight/2);
-  //console.log(viewportHeight);
-  var second = document.getElementById("second");
-  second.style.height = viewportHeight+"px";
-  second.style.transform = "translateY("+viewportHeight+"px)";
   window.onscroll = runScroller;
 }
 
